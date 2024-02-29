281ac2fde106995eb13b1d557b12b8eedf3bb964
Lex Miin <alexmiyin@gmail.com>
2024-03-01 00:40:44 +0200

    feat: run a simple script on host via ssh

---
+++ cmd/deploy/main.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"io"
+
+	"log"
+	"os"
+
+	"golang.org/x/crypto/ssh"
+	"golang.org/x/crypto/ssh/knownhosts"
+)
+
+func NewSSHClient() *ssh.Client {
+	hostkeyCallback, err := knownhosts.New("/Users/lex/.ssh/known_hosts")
+	if err != nil {
+		log.Fatalf("error reading known_hosts file: %v\n", err)
+	}
+
+	key, err := os.ReadFile("/Users/lex/.ssh/id_rsa")
+	if err != nil {
+		log.Fatalf("error reading id_rsa file: %v\n", err)
+	}
+
+	signer, err := ssh.ParsePrivateKey(key)
+	if err != nil {
+		log.Fatalf("error parsing private key: %v\n", err)
+	}
+
+	config := &ssh.ClientConfig{
+		User: "root",
+		Auth: []ssh.AuthMethod{
+			ssh.PublicKeys(signer),
+		},
+		HostKeyCallback: hostkeyCallback,
+	}
+
+	client, err := ssh.Dial("tcp", "95.216.170.196:22", config)
+	if err != nil {
+		log.Fatalf("unable to connect: %v\n", err)
+	}
+
+	return client
+
+}
+
+func main() {
+	client := NewSSHClient()
+	defer func() {
+		err := client.Close()
+		if err != nil {
+			log.Fatalf("error closing client: %v\n", err)
+		}
+	}()
+
+	for range 3 {
+		session, err := client.NewSession()
+		if err != nil {
+			log.Fatalf("error creating new session: %v\n", err)
+		}
+
+		session.Stdout = os.Stdout
+
+		if err := session.Run("/root/prog.sh"); err != nil {
+			log.Printf("error running command: %v\n", err)
+			continue
+		}
+
+		err = session.Close()
+		if err != nil && err != io.EOF {
+			log.Printf("error closing session: %v\n", err)
+		}
+
+	}
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module neite.dev/go-deploy
 
 go 1.22.0
+
+require (
+	golang.org/x/crypto v0.20.0 // indirect
+	golang.org/x/sys v0.17.0 // indirect
+)
