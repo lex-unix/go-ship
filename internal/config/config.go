d46776a6ed07b82b31ac45500e3950b13ee3b66a
Lex Miin <alexmiyin@gmail.com>
2024-03-05 13:27:40 +0200

    fix: return error to the caller and use cwd
    - propagate error to the caller
    - use working directory to construct path to config file
    - use embeded user config template
    - add `IsExists()` helper to check if config file already exists
    
    fixes #6

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,11 +1,19 @@
 package config
 
 import (
-	"log"
+	"embed"
 	"os"
+	"path"
 
 	"gopkg.in/yaml.v3"
 )
+
+var (
+	userConfigFilename = "goship.yaml"
+)
+
+//go:embed templates/*
+var templateFS embed.FS
 
 type SSH struct {
 	User    string `yaml:"user"`
@@ -29,36 +37,67 @@ type UserConfig struct {
 	Registry *Registry `yaml:"registry"`
 }
 
-// Reads user's config file into UserConfig struct
-func ReadConfig() *UserConfig {
-	template, err := os.ReadFile("./newConf.yaml")
+// ReadConfig reads user's config file into UserConfig struct
+func ReadConfig() (*UserConfig, error) {
+	configPath, err := getConfigPath()
 	if err != nil {
-		log.Fatalf("error reading file: %v", err)
+		return nil, err
+	}
+	template, err := os.ReadFile(configPath)
+	if err != nil {
+		return nil, err
 	}
 
 	var config UserConfig
 
 	err = yaml.Unmarshal(template, &config)
 	if err != nil {
-		log.Fatalln(err)
+		return nil, err
 	}
-
-	return &config
+	return &config, nil
 }
 
-// Creates new config file for the user to fill out
-func NewConfig() {
-	template, err := os.ReadFile("./cmd/config/templates/config.yaml")
+// NewConfig creates new config file from a template
+func NewConfig() error {
+	template, err := templateFS.ReadFile("templates/user_config.yaml")
 	if err != nil {
-		log.Fatalf("error reading file: %v", err)
+		return err
 	}
 
-	dest, err := os.Create("./newConf.yaml")
+	configPath, err := getConfigPath()
 	if err != nil {
-		log.Fatalf("error creating file: %v", err)
+		return err
 	}
 
+	dest, err := os.Create(configPath)
+	if err != nil {
+		return err
+	}
 	defer dest.Close()
 
 	dest.Write(template)
+
+	return nil
+}
+
+// IsExists checks if the config file exists
+func IsExists() bool {
+	configPath, err := getConfigPath()
+	if err != nil {
+		return false
+	}
+	if _, err := os.Stat(configPath); err != nil {
+		return false
+	}
+
+	return true
+}
+
+func getConfigPath() (string, error) {
+	cwd, err := os.Getwd()
+	if err != nil {
+		return "", err
+	}
+	configPath := path.Join(cwd, userConfigFilename)
+	return configPath, nil
 }
