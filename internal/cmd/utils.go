fb297cc007a3dbdb19605e65b6d7ba779cf9aa6e
Lex Miin <alexmiyin@gmail.com>
2024-03-08 21:53:18 +0200

    feat: get latest commit hash function

---
+++ internal/cmd/utils.go
@@ -0,0 +1,18 @@
+package cmd
+
+import (
+	"bytes"
+	"fmt"
+	"os/exec"
+)
+
+func latestCommitHash() (string, error) {
+	c := exec.Command("git", "rev-parse", "--short", "HEAD")
+	out, err := c.Output()
+	if err != nil {
+		fmt.Println(err)
+		return "", err
+	}
+	out = bytes.TrimSpace(out)
+	return string(out), nil
+}
