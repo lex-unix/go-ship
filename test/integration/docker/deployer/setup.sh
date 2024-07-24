b18eda4d382581492ff243049389df66c15b6999
Lex Miin <alexmiyin@gmail.com>
2024-07-24 15:25:33 +0300

    test: use registry instead of volume as cache for test app image

--- test/integration/docker/deployer/setup.sh
+++ test/integration/docker/deployer/setup.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
-install_goship() {
-    cd /go-ship && go build -o /usr/local/bin/goship ./cmd/goship/
+cd /go-ship && go build -o /usr/local/bin/goship ./cmd/goship/
+
+push_to_registry() {
+    if ! stat /registry/docker/registry/v2/repositories/$1/_manifests/tags/$2/current/link > /dev/null; then
+        hub_tag=$1:$2
+        registry_4443_tag=registry:4443/$1:$2
+        docker pull $hub_tag
+        docker tag $hub_tag $registry_4443_tag
+        docker push $registry_4443_tag
+    fi
 }
 
-install_goship
+push_to_registry node 20.15.1-slim
 
+rm -f /root/.ssh/known_hosts
 ssh-keyscan -H vm1 >> /root/.ssh/known_hosts
 ssh-keyscan -H vm2 >> /root/.ssh/known_hosts
